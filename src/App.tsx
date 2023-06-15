4944bbbba6438f367ea3fd0227aac398e6893f7e
Nikita <nikitasharma2999@gmail.com>
2023-06-15 14:07:59 +0530

    update to tsx, basic setup

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import TopicsList from "./components/TopicsList";
+
+interface TopicData {
+  id: number;
+  category: string;
+  name: string;
+  keywords: string[];
+}
+
+const App: React.FC = () => {
+  const [topics, setTopics] = useState<TopicData[]>([]);
+
+  const handleAddTopic = (
+    name: string,
+    category: string,
+    keywords: string[]
+  ) => {
+    const newTopic: TopicData = {
+      id: Date.now(),
+      category,
+      name,
+      keywords,
+    };
+    setTopics([...topics, newTopic]);
+  };
+
+  const handleDeleteTopic = (id: number) => {
+    const updatedTopics = topics.filter((topic) => topic.id !== id);
+    setTopics(updatedTopics);
+  };
+
+  const handleGenerateBlog = (tone: string) => {
+    // Logic for generating the blog based on the selected tone
+    console.log(`Generating blog with tone: ${tone}`);
+  };
+
+  return (
+    <div>
+      <h1>TopicHub</h1>
+      <TopicsList topics={topics} onDelete={handleDeleteTopic} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TopicsList.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+interface TopicData {
+  id: number;
+  category: string;
+  name: string;
+  keywords: string[];
+}
+
+interface TopicsListProps {
+  topics: TopicData[];
+  onDelete: (id: number) => void;
+}
+
+const TopicsList: React.FC<TopicsListProps> = ({ topics, onDelete }) => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const uniqueCategories = topics.reduce((categories: string[], topic) => {
+    if (!categories.includes(topic.category)) {
+      categories.push(topic.category);
+    }
+    return categories;
+  }, []);
+
+  const filteredTopics = selectedCategory
+    ? topics.filter((topic) => topic.category === selectedCategory)
+    : topics;
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <div className="filter-bar">
+        <label htmlFor="category-filter">Filter by Category:</label>
+        <select
+          id="category-filter"
+          value={selectedCategory}
+          onChange={(e) => handleCategoryChange(e.target.value)}
+        >
+          <option value="">All</option>
+          {uniqueCategories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default TopicsList;
